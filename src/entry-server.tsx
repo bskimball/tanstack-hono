55f8955b2c696741e1160355dc77852fadbbafb5
bskimball <briankimball1982@gmail.com>
2025-08-20 10:31:52 -0400

    updates

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -56,6 +56,7 @@ app.get("*", async (c) => {
 				<html lang="en">
 					<head>
 						<meta charSet="UTF-8" />
+						<link rel="icon" type="image/svg+xml" href="/logo.svg" />
 						<meta
 							name="viewport"
 							content="width=device-width, initial-scale=1.0"
